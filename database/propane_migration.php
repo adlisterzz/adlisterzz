d64e9f995732fc2e7cd438608d1cad696ef49ddb
stanholzendorf <sholzendorf@mail.com>
2016-03-31 10:36:33 -0500

    Created Database Migration and Seeder Files

--- database/propane_migration.php
+++ database/propane_migration.php
@@ -1,3 +1,25 @@
 <?php
-// Migration to follow!
+DEFINE('DB_PASS', 'codeup1');
+DEFINE('DB_USER', 'codeup1');
+DEFINE('DB_NAME', 'dbname=ad');
+DEFINE('DB_HOST', 'mysql:host=127.0.0.1;');
+
+require 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$droptable = 'DROP TABLE IF EXISTS national_parks';
+$dbc->exec($droptable);
+
+$createtable = 'CREATE TABLE propane_and_accessories(
+				id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+				name VARCHAR(200) NOT NULL,
+				maker VARCHAR(200) NOT NULL,
+				grade VARCHAR(100) NOT NULL,
+				type VARCHAR(100) NOT NULL,
+				price FLOAT(8,2) UNSIGNED NOT NULL,
+				description VARCHAR(450) NOT NULL,
+				PRIMARY KEY(id)
+				)';
+$dbc->exec($createtable);
 ?>
--- database/propane_seeder.php
+++ database/propane_seeder.php
@@ -1,3 +1,44 @@
 <?php
-// This will be the seeder!
+
+DEFINE('DB_PASS', 'codeup1');
+DEFINE('DB_USER', 'codeup1');
+DEFINE('DB_NAME', 'dbname=ad');
+DEFINE('DB_HOST', 'mysql:host=127.0.0.1;');
+
+require 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$deleteRecords = 'TRUNCATE national_parks';
+$dbc->exec($deleteRecords);
+
+
+$propanes = array(
+
+	array('name'=> 'Testname', 'make'=> 'Testmake', 'grade'=> 'Testgrade', 'type'=> 'Testtype', 'price'=> 1000, 'description'=> 'Some description')
+
+
+
+	);
+
+foreach ($propanes as $propane) {
+
+		$query = "INSERT INTO propane_and_accessories (name, make, grade, type, price, description)
+			   VALUES (:name, :make, :grade, :type, :price, :description)";
+
+
+
+$statement = $dbc->prepare($query);
+$statement->bindValue(':name', $propane['name'], PDO::PARAM_STR);
+$statement->bindValue(':make', $propane['make'], PDO::PARAM_STR);
+$statement->bindValue(':grade', $propane['garde'], PDO::PARAM_STR);
+$statement->bindValue(':type', $propane['type'], PDO::PARAM_STR);
+$statement->bindValue(':price', $propane['price'], PDO::PARAM_STR);
+$statement->bindValue(':description', $propane['description'], PDO::PARAM_STR);
+			   		  
+
+
+$statement->execute();
+echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
+}
 ?>
