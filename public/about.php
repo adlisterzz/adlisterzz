264da0cea2c6bfe22a975df77970e3ffc664c052
GBickham <Bickham@macs-MacBook-Pro.local>
2016-04-01 10:26:10 -0500

    Add About page and bio pics

---
+++ public/about.php
@@ -0,0 +1,124 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Strickland's Propane and Propane Accessories</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/carousel.css" rel="stylesheet">
+    <link href="/css/index.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>About Page</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+    </div>
+       
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="column">
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/BuckStrickland.png" width="140" height="140">
+          <h2>Owner</h2>
+          <h3>Buck Strickland</h3>
+          <p> Choosing the right fuel for the right job is always important. Whether you're overcooking last years freezer-burned deer venison or powering a generator in your remote greenhouse, we've got you covered. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/Hank_Hill.png" width="140" height="140">
+          <h2>Assistant Manager</h2>
+          <h3>Hank Hill</h3>
+          <p> You have propane. We have accessories. Camping? RVing? Does a friend need to hold your beer for this one? </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/JoeJack.jpg" width="140" height="140">
+          <h2>Joe Jack</h2>
+          <p> Choosing the right fuel for the right job is always important. Whether you're overcooking last years freezer-burned deer venison or powering a generator in your remote greenhouse, we've got you covered. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/Enrique.jpg" width="140" height="140">
+          <h2>Enrique</h2>
+          <p> You have propane. We have accessories. Camping? RVing? Does a friend need to hold your beer for this one? </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/Donna2.jpg" width="140" height="140">
+          <h2>Donna</h2>
+          <p> Choosing the right fuel for the right job is always important. Whether you're overcooking last years freezer-burned deer venison or powering a generator in your remote greenhouse, we've got you covered. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        
+        <div class="col-md-2">
+          <img class="img-circle" src="/img/Booda_Sack.png" width="140" height="140">
+          <h2>Roger Sack</h2>
+          <p> You have propane. We have accessories. Camping? RVing? Does a friend need to hold your beer for this one? </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
+  </body>
+  </html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,12 +15,10 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Carousel Template for Bootstrap</title>
+    <title>Strickland's Propane and Propane Accessories</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-
-
 
     <!-- Custom styles for this template -->
     <link href="/css/carousel.css" rel="stylesheet">
@@ -40,21 +43,28 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <br>
                 <li class="active"><a href="#">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
+                
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jump to a section!<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
+                    <li><a href="#">Propane</a></li>
+                    <li><a href="#">Propane Bottles</a></li>
+                    <li><a href="#">Propane Regulators</a></li>
+                    <li><a href="#">Propane Hoses</a></li>
                     <li role="separator" class="divider"></li>
                     <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
+                    <li><a href="#">Strickland Gift Shop</a></li>
+                    <li><a href="#">Check out the Propaniacs</a></li>
+                  <form class="navbar-form navbar-right" role="search"><div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search"></div>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                  </form>
                   </ul>
+
+                  
                 </li>
               </ul>
             </div>
@@ -86,7 +96,7 @@
           </div>
         </div>
         <div class="item">
-          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+          <img class="second-slide" src="/img/Strickland_Propane.jpg" alt="Second slide">
           <div class="container">
             <div class="carousel-caption">
               <h1>Propane and Propane Accessories Since 1973</h1>
@@ -96,7 +106,7 @@
           </div>
         </div>
         <div class="item">
-          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <img class="third-slide" src="/img/Propaniacs.jpeg" alt="Third slide">
           <div class="container">
             <div class="carousel-caption">
               <h1>Don't miss out on our annual "Grillstravanganza"!</h1>
@@ -128,13 +138,13 @@
         <div class="col-lg-4">
           <img class="img-circle" src="/img/propaneBlueFlameCircle.jpg" width="140" height="140">
           <h2>Propane</h2>
-          <p> Choosing the right fuel for the right job is always important. Whether you're overcooking  </p>
+          <p> Choosing the right fuel for the right job is always important. Whether you're overcooking last years freezer-burned deer venison or powering a generator in your remote greenhouse, we've got you covered. </p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/img/PropaneAccessories.jpeg" width="140" height="140">
           <h2>Propane Accessories</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p> You have propane. We have accessories. Camping? RVing? Does a friend need to hold your beer for this one? </p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
@@ -145,12 +155,12 @@
       <hr class="featurette-divider">
 
       <div class="row featurette">
-        <div class="col-md-7">
+        <div class="col-lg-7">
           <h2 class="featurette-heading">How to pick the right propane for the job.<span class="text-muted"> It'll blow your mind.</span></h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        <div class="col-lg-5">
+          <img class="featurette-image img-responsive center-block" src="/img/PropaneFlame.jpeg" alt="Hot!">
         </div>
       </div>
 
@@ -162,7 +172,7 @@
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          <img class="featurette-image img-responsive center-block" src="/img/PropaneLife.jpeg" alt="Generic placeholder image">
         </div>
       </div>
 
@@ -170,11 +180,11 @@
 
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading"> Let everyone know you're part of the hottest new scene. <span class="text-muted">Checkmate.</span></h2>
+          <h2 class="featurette-heading"> Done acceossirizing your propane? Ready to accessorize your body? <span class="text-muted">Checkmate.</span></h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          <img class="featurette-image img-responsive center-block" src="/img/stricklandShirt.jpeg" alt="Generic placeholder image">
         </div>
       </div>
 
