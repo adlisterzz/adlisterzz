9090eb1f1e4cdcc3872837ddb67840a3167201b1
GBickham <Bickham@macs-MacBook-Pro.local>
2016-04-05 14:32:01 -0500

    finished footer

--- public/stricklandFooter.php
+++ public/stricklandFooter.php
@@ -4,19 +4,23 @@
 
         	<form class="navbar-form navbar-left" role="search">
         		<div class="form-group">
-          			<input type="text" class="form-control" placeholder="Search">
-        		</div>
-        		<button type="submit" class="btn btn-default">Submit</button>
+
+          				<input type="text" class="form-control" id="forcedInputSize" placeholder="Search">
+
+           		</div>
+           		<button type="submit" class="btn btn-default">Submit</button>
       		</form>
 
-      		<ul class="nav navbar-nav social center-block col-md-4 col-md-offset-4">
+      		<a class="navbar-right addPadding" href="http://www.ad-lister.dev"><img src="/img/strickland4.jpeg"></a>
+
+      		<ul class="nav navbar-nav center-block centeredMediaButtons">
             	<li><a href="http://www.facebook.com" target="_blank"><i class="fa fa-lg fa-facebook"></i></a></li>
             	<li><a href="http://www.twitter.com" target="_blank"><i class="fa fa-lg fa-twitter"></i></a></li>
             	<li><a href="http://www.facebook.com" target="_blank"><i class="fa fa-lg fa-google-plus"></i></a></li>
             	<li><a href="http://www.linkedin.com" target="_blank"><i class="fa fa-lg fa-linkedin"></i></a></li>
         	</ul>
       		
-      		<a class="navbar-right" href="http://www.ad-lister.dev"><img src="/img/strickland4.jpeg"></a>
+      		
 
       	</div>
 	</footer>
