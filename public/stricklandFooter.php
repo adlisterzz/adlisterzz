af4f56dcc24ef67d66fbbd8685f3b7d4a0e61350
GBickham <Bickham@macs-MacBook-Pro.local>
2016-04-05 11:00:11 -0500

    minor change

--- public/stricklandFooter.php
+++ public/stricklandFooter.php
@@ -1,7 +1,7 @@
-<footer class="footer">
-      <div class="container">
+<footer class="navbar-footer">
+      <div class="container-fluid">
         <p>
-        	<form class="nav navbar-nav navbar-fixed-bottom navbar-form navbar-left" role="search">
+        	<form class="nav navbar-nav navbar-fixed-bottom navbar-form" role="search">
         		<div class="form-group">
           			<input type="text" class="form-control" placeholder="Search">
         		</div>
@@ -9,5 +9,6 @@
       		</form>
         </p>
       </div>
+      <a class="navbar-right" href="http://www.ad-lister.dev"><img src="/img/strickland4.jpeg"></a>
     </footer>
 
